1b139e965f4d453687aceb881f2b2c8ec51933ff
ThoSe1990 <thomas.sedlmair@googlemail.com>
2023-11-10 18:41:12 -0500

    updated consumer/packages

--- package/conanfile.py
+++ package/conanfile.py
@@ -1,22 +1,76 @@
 from conan import ConanFile
-from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
-from conan.tools.files import get
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+from conan.tools.files import get, copy
+
+from os.path import join 
 
 
 class CucumberRecipe(ConanFile):
 
-    name = "cwt-cucumber"
+  name = "cwt-cucumber"
 
-    license = "MIT"
-    author = "Thomas Sedlmair, thomas.sedlmair@googlemail.com"
-    url = "https://github.com/ThoSe1990/cwt-cucumber"
-    description = "A simple C/C++ Cucumber interpreter"
-    topics = ("cpp", "bdd", "testing", "cucumber")
+  license = "MIT"
+  author = "Thomas Sedlmair, thomas.sedlmair@googlemail.com"
+  url = "https://github.com/ThoSe1990/cwt-cucumber"
+  description = "A simple C/C++ Cucumber interpreter"
+  topics = ("cpp", "bdd", "testing", "cucumber")
 
-    settings = "os", "compiler", "build_type", "arch"
-    options = {"no_main": [True, False], "stack_trace": [True, False]}
-    default_options = {"no_main": False, "stack_trace": False}
+  generators = "CMakeDeps"
+  settings = "os", "compiler", "build_type", "arch"
+  options = {"stack_trace": [True, False]}
+  default_options = {"stack_trace": False}
 
-    def source(self):
-        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+  def source(self):
+    get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
+  def generate(self):
+    tc = CMakeToolchain(self)
+    tc.variables["STACK_TRACE"] = bool(self.options.stack_trace)
+    tc.generate()
+
+  def build(self):
+    cmake = CMake(self)
+    cmake.configure()
+    cmake.build()
+
+  def package(self):
+    dst = join(self.package_folder, "include", "cwt")
+    src = join(self.source_folder, "src", "cwt")
+    copy(self, "*.h", src, dst, keep_path=False, excludes="implementation")
+    copy(self, "*.hpp", src, dst, keep_path=False, excludes="implementation")
+    
+    dst = join(self.package_folder, "lib")
+    src = join(self.source_folder, "lib")
+    copy(self, "*", src, dst)
+
+
+  def package_info(self):
+    self.cpp_info.includedirs = ['include'] 
+    self.cpp_info.libdirs = ['lib']
+
+    self.cpp_info.components["cucumber"].libs = ["cucumber-cpp"]
+    self.cpp_info.components["cucumber-c"].libs = ["cucumber"]
+    self.cpp_info.components["cucumber-no-main"].libs = ["cucumber-cpp-nomain"]
+
+    # self.cpp_info.set_property("cmake_file_name", "cwt")
+    # self.cpp_info.set_property("cmake_target_name", "cwt-cucumber::cucumber")
+    self.cpp_info.components["cucumber"].set_property("cmake_target_name", "cwt::cucumber")
+    self.cpp_info.components["cucumber-c"].set_property("cmake_target_name", "cwt::cucumber-c")
+    self.cpp_info.components["cucumber-no-main"].set_property("cmake_target_name", "cwt::cucumber-no-main")
+
+
+      # self.cpp_info.set_property("cmake_file_name", "cwt-cucumer")
+      # self.cpp_info.set_property("cmake_find_mode", "both")
+      # self.cpp_info.libs = ["libcucumber-cpp"]
+      # self.cpp_info.set_property("cmake_target_name", "cwt-cucumber::cucumber")
+
+      # self.cpp_info.components["cucumber"].libs = ['libcucumber-cpp']
+      # self.cpp_info.components["cucumber"].set_property("cmake_target_name", "cucumber")
+
+
+      # self.cpp_info.components["cucumber-cpp"].set_property("cmake_target_name", "cwt-cucumber::cucumber")
+      # self.cpp_info.components["cucumber-cpp"].set_property("pkg_config_name", "cucumber")
+      # self.cpp_info.components["cucumber-cpp"].libs = ['libcucumber-cpp']
+
+
+        
